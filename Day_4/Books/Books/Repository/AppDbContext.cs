3fa578dabcf7cbbd9d7217ffa60b0f311a7b8bb6
HarshDhimmar2804 <harsh8487861901@gmail.com>
2024-05-23 23:39:52 +0530

    adding day-4 task

---
+++ Day_4/Books/Books/Repository/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Books.Repository.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books.Repository
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ Day_4/Books/Books/Services/BooksService.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Books.Repository;
+using Books.Repository.Entities;
+
+namespace Books.Services
+{
+    public class BooksService
+    {
+        private readonly AppDbContext _cIDbContext;
+        public BooksService(AppDbContext cIDbContext)
+        {
+            _cIDbContext = cIDbContext;
+        }
+
+        public List<Book> GetAll()
+        {
+            return _cIDbContext.Books.ToList();
+        }
+
+        public Book GetById(int id) => _cIDbContext.Books.FirstOrDefault(b => b.Id == id);
+
+        public void Add(Book book)
+        {
+            _cIDbContext.Books.Add(book);
+            _cIDbContext.SaveChanges();
+        }
+
+         public void Update(Book book)
+        {
+            var existingBook = _cIDbContext.Books.FirstOrDefault(b => b.Id == book.Id);
+            if (existingBook != null)
+            {
+                existingBook.Title = book.Title;
+                existingBook.Author = book.Author;
+                _cIDbContext.SaveChanges();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var book = GetById(id);
+            if (book != null)
+            {
+                _cIDbContext.Books.Remove(book);
+                _cIDbContext.SaveChanges(); 
+            }
+        }
+    }
+}
